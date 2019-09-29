a0b1023a8f15712d33b9e7c963fe42600296c014
Ruth Vazquez <rthvazquez@gmail.com>
2019-09-28 22:45:09 -0700

    started the server and orms files

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,35 @@
-//set up the code to connect Node to MYSQL
-//export the connection
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection;
+// = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+  	port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  })
+}
+
+// Make connection.
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+connection.connect();
+// Export connection for our ORM to use.
+module.exports = connection
